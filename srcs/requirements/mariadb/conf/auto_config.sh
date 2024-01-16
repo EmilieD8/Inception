647fc0bc99ec4d40eccc4daed1edc104af1d522d
edrouot <emilie.drouot8@gmail.com>
2024-01-16 14:23:23 +0100

    inception

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-service mysql start;
-
-echo "Data base creation..."
-
 MYSQL_HOSTNAME="mariadb"
 MYSQL_DATABASE="inception"
 MYSQL_USER="edrouot"
 MYSQL_PASSWORD="incep_pass123"
 MYSQL_ROOT_PASSWORD="incep_root_pass123"
 
+service mysql start;
+echo "Data base creation..."
+
+mysql_status=$(service mysql status)
+
+# Echo the status
+echo "MySQL Status: $mysql_status"
 # mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
 # mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -22,16 +25,19 @@ MYSQL_ROOT_PASSWORD="incep_root_pass123"
 
 # mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_name.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}_name\`.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mysql < db1.sql
+mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" < db1.sql
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+mysql_status=$(service mysql status)
 
-mysqld
+# Echo the status
+echo "MySQL Status: $mysql_status"
+
+#mysqld
 # exec mysqld_safe
