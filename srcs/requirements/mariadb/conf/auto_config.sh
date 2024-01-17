efe50bde34a0853174e47427d305b8caffebae9f
EmilieD8 <103639810+EmilieD8@users.noreply.github.com>
2024-01-17 17:24:12 +0100

    Update auto_config.sh

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,91 +1,13 @@
-# #!/bin/bash
-
-# MYSQL_HOSTNAME="mariadb"
-# MYSQL_DATABASE="inception"
-# MYSQL_USER="edrouot"
-# MYSQL_PASSWORD="incep_pass123"
-# MYSQL_ROOT_PASSWORD="incep_root_pass123"
-
-# service mysql start;
-# echo "Data base creation..."
-
-# mysql_status=$(service mysql status)
-
-# # Echo the status
-# echo "MySQL Status: $mysql_status"
-# # mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-
-# # mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# # mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-# # mysql -e "FLUSH PRIVILEGES;"
-
-# # mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-
-# echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > db1.sql
-# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-# echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}_name\`.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
-# echo "FLUSH PRIVILEGES;" >> db1.sql
-
-# mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" < db1.sql
-# mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-
-# mysql_status=$(service mysql status)
-
-# # Echo the status
-# echo "MySQL Status: $mysql_status"
-
-# #mysqld
-# # exec mysqld_safe
-
 #!/bin/bash
 
 sleep 5;
 
-MYSQL_HOSTNAME="mariadb"
-MYSQL_DATABASE="inception"
-MYSQL_USER="edrouot"
-MYSQL_PASSWORD="incep_pass123"
-MYSQL_ROOT_PASSWORD="incep_root_pass123"
-
 service mysql start;
-echo "Data base creation..."
 
-mysql_status=$(service mysql status)
-
-# Echo the status
-echo "MySQL Status: $mysql_status"
-mysql -uroot -p"rootpass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-mysql -uroot -p"rootpass" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-
-mysql -uroot -p"rootpass" -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-mysql -uroot -p"rootpass" -e "FLUSH PRIVILEGES;"
-
-# mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-
-# echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > db1.sql
-# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-# echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}_name\`.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
-# echo "FLUSH PRIVILEGES;" >> db1.sql
-
-# mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" < db1.sql
-# mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-
-mysql_status=$(service mysql status)
-
-# Echo the status
-echo "MySQL Status: $mysql_status"
-
-#mysqld
-# exec mysqld_safe
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
 exec "$@"
